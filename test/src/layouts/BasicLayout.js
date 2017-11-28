ab70d2b42bfdefd70b83b336740aaf2b45726d81
A12 <a12@A12s-MacBook-Air.local>
2017-11-28 20:32:49 +0800

    ant design pro(练习)

---
+++ test/src/layouts/BasicLayout.js
@@ -0,0 +1,118 @@
+/**
+ * 2017-11.27 ligang
+ */
+
+import React from 'react';
+import { Layout, Menu, Icon } from 'antd';
+import { connect } from 'dva';
+import { ContainerQuery } from 'react-container-query';
+import classNames from 'classnames';
+import styles from './BasicLayout.less';
+
+const { Header, Sider, Content, Footer } = Layout;
+
+const query = {
+  'screen-xs': {
+    maxWidth: 575,
+  },
+  'screen-sm': {
+    minWidth: 576,
+    maxWidth: 767,
+  },
+  'screen-md': {
+    minWidth: 768,
+    maxWidth: 991,
+  },
+  'screen-lg': {
+    minWidth: 992,
+    maxWidth: 1199,
+  },
+  'screen-xl': {
+    minWidth: 1200,
+  },
+};
+
+// const SubMenu = Menu.SubMenu;
+const { SubMenu } = Menu;
+
+class BasicLayout extends React.Component {
+
+  render() {
+    const layout = (
+      <Layout>
+        <Sider className={styles.left} style={{ width: '500px' }}>
+          <div className={styles.logo}>
+            <a href="#/">
+              <img src="https://gw.alipayobjects.com/zos/rmsportal/iwWyPinUoseUxIAeElSx.svg" alt="logo" />
+              <h1>Ant Design Pro</h1>
+            </a>
+          </div>
+          <Menu
+            mode="inline"
+            // openKeys={this.state.openKeys}
+            onOpenChange={this.onOpenChange}
+            style={{ width: 200 }}
+          >
+            <SubMenu key="sub1" title={<span><Icon type="clock-circle-o" /><span>Dashboard</span></span>}>
+              <Menu.Item key="1">分析页</Menu.Item>
+              <Menu.Item key="2">监控页</Menu.Item>
+              <Menu.Item key="3">工作台</Menu.Item>
+              {/* <Menu.Item key="4">Option 4</Menu.Item> */}
+            </SubMenu>
+            <SubMenu key="sub2" title={<span><Icon type="edit" /><span>表单页</span></span>}>
+              <Menu.Item key="4">基础表单</Menu.Item>
+              <Menu.Item key="5">分步表单</Menu.Item>
+              <Menu.Item key="6">高级表单</Menu.Item>
+              {/* <SubMenu key="sub3" title="Submenu">
+                <Menu.Item key="8">查询表格</Menu.Item>
+                <Menu.Item key="9">标准列表</Menu.Item>
+                <Menu.Item key="8">卡片列表</Menu.Item>
+                <Menu.Item key="9">搜索列表(项目)</Menu.Item>
+                <Menu.Item key="8">搜索列表(应用)</Menu.Item>
+                <Menu.Item key="9">搜索列表(文章)</Menu.Item>
+              </SubMenu> */}
+            </SubMenu>
+            <SubMenu key="sub3" title={<span><Icon type="exception" /><span>列表页</span></span>}>
+              <Menu.Item key="7">查询表格</Menu.Item>
+              <Menu.Item key="8">标准列表</Menu.Item>
+              <Menu.Item key="9">卡片列表</Menu.Item>
+              <Menu.Item key="10">搜索列表(项目)</Menu.Item>
+              <Menu.Item key="11">搜索列表(应用)</Menu.Item>
+              <Menu.Item key="12">搜索列表(文章)</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub4" title={<span><Icon type="layout" /><span>详情页</span></span>}>
+              <Menu.Item key="9">基础详情</Menu.Item>
+              <Menu.Item key="10">高级详情</Menu.Item>
+              {/* <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item> */}
+            </SubMenu>
+            <SubMenu key="sub5" title={<span><Icon type="check-circle-o" /><span>结果</span></span>}>
+              <Menu.Item key="9">成功</Menu.Item>
+              <Menu.Item key="10">失败</Menu.Item>
+              <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub6" title={<span><Icon type="exclamation-circle-o" /><span>异常</span></span>}>
+              <Menu.Item key="9">Option 9</Menu.Item>
+              <Menu.Item key="10">Option 10</Menu.Item>
+              <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9"><Icon type="file-text" /><span><a href="https://www.baidu.com/">使用文档</a></span></Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className={styles.right}>
+          <Header className={styles.header}>header</Header>
+          <Content className={styles.content}>main content</Content>
+          <Footer className={styles.footer}>footer</Footer>
+        </Layout>
+      </Layout>
+    );
+    return (
+      <ContainerQuery query={query}>
+        {params => <div className={classNames(params)}>{layout}</div>}
+      </ContainerQuery>
+    );
+  }
+}
+export default connect()(BasicLayout);
---
+++ test/src/router.js
@@ -0,0 +1,23 @@
+/**
+ * 2017-11.27 ligang
+ */
+
+import React from 'react';
+import { Router, Route, Switch } from 'dva/router';
+// import IndexPage from './routes/IndexPage';
+// import Products from './routes/Products'; // 加
+import BasicLayout from './layouts/BasicLayout'; // 加
+
+function RouterConfig({ history }) {
+  return (
+    <Router history={history}>
+      <Switch>
+        {/* <Route path="/" exact component={Products} /> */}
+        <Route path="/" exact component={BasicLayout} />
+        {/* <Route path="/products" exact component={Products} /> */}
+      </Switch>
+    </Router>
+  );
+}
+
+export default RouterConfig;
