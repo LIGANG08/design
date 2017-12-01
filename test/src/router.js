50ddcffd97bb9a7ed9da4ad3ff3953d2dd98a7ed
A12 <a12@A12s-MacBook-Air.local>
2017-12-01 20:41:10 +0800

    design

--- test/src/router.js
+++ test/src/router.js
@@ -7,6 +7,7 @@ import { Router, Route, Switch } from 'dva/router';
 // import IndexPage from './routes/IndexPage';
 // import Products from './routes/Products'; // 加
 import BasicLayout from './layouts/BasicLayout'; // 加
+// import about from './layouts/test';
 // import test from './layouts/test'; // 加
 
 function RouterConfig({ history }) {
@@ -15,7 +16,7 @@ function RouterConfig({ history }) {
       <Switch>
         {/* <Route path="/" exact component={Products} /> */}
         <Route path="/" exact component={BasicLayout} />
-        {/* <Route path="/products" exact component={Products} /> */}
+        {/* <Route path="/" exact component={test} /> */}
       </Switch>
     </Router>
   );
